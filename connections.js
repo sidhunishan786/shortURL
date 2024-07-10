967fc8d9d87ed8a0fbffc56142ecb59affb823b4
nishan singh <sidhunishan786@gmail.com>
2024-07-10 20:33:12 +0530

    10JULY2024

---
+++ connections.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+export async function connectToMongoDB(){
+    return await mongoose.connect(process.env.DBURL);
+}
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from "express";
 import 'dotenv/config'
-
+import { connectToMongoDB } from "./connections.js";
 
 console.log(process.env.PORT);
+connectToMongoDB().then(()=>{
+    console.log(`connected to ${process.env.DBURL}`);
+})
+.catch((err)=>{
+    console.log(`Error is ${err}`);
+
+})
 
 
 
 
 
-express().listen()
+express().listen(process.env.DBURL,()=>{
+    console.log(`server started at ${process.env.PORT}`);
+})
